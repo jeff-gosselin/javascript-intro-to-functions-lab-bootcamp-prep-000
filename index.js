be44edc42c0bbf0037b9e52716bdd07df3a5fee3
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-11 05:24:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!";
   }
   
-  if (string === string.toLowerCase()) {
+  if (string === string.toUpperCase()) {
     return "I can\'t hear you!";
   }
 }
